bd91cb780e67e0d62494645cc669018c440a78af
alexeykomov <alexeykcontact@gmail.com>
2017-05-23 19:48:34 +0300

    edit: webroot path in certbot.

--- 01-install-letsencrypt.sh
+++ 01-install-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 service nginx start -d
-certbot certonly -n --webroot --webroot-path=/usr/share/nginx/html -d whisp.space -d www.whisp.space --agree-tos --email "alexeykcontact@gmail.com"
+certbot certonly -n --webroot --webroot-path=/var/www/html -d whisp.space -d www.whisp.space --agree-tos --email "alexeykcontact@gmail.com"
 service nginx reload
 crontab /etc/my_init.d/crontab-changed
