76596393517ec34bf039ba634639f3ff50ca3fb4
alexeykomov <alexeykofficial@gmail.com>
2017-05-27 14:11:08 +0300

    edit: nginx confs

--- 01-install-letsencrypt.sh
+++ 01-install-letsencrypt.sh
@@ -2,5 +2,7 @@
 openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 service nginx start -d
 certbot certonly -n --webroot --webroot-path=/var/www/html -d whisp.space -d www.whisp.space --agree-tos --email "alexeykcontact@gmail.com"
-service nginx reload
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.backup.conf
+mv /etc/nginx/nginx.ssl.conf /etc/nginx/nginx.conf
+service nginx reload -d
 crontab /etc/my_init.d/crontab-changed
